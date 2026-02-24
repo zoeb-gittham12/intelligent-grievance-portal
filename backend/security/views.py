15a7d9c8f942ec9bdeec020883a94e376023f88c
Megh Patel <patelmegh245@gmail.com>
2026-02-24 21:43:10 +0530

    Today's Work

--- backend/security/views.py
+++ backend/security/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import UserProfile
+from rest_framework.permissions import AllowAny
+
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login_view(request):
     username = request.data.get("username")
     password = request.data.get("password")
